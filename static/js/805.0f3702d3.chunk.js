cb33818a889a94b4cf22405244b3f0fa537b3315
Vasyl94 <110615025+Vasyl94@users.noreply.github.com>
2023-08-05 09:07:22 +0000

    Deploying to gh-pages from @ Vasyl94/goit-react-hw-05-movies@0e1c820af465ecb4cf0682f5278d930eb70a5c63 🚀

---
+++ static/js/805.0f3702d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,r,t){t.r(r);var n=t(184);r.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Sorry page not found"})})}}}]);
+//# sourceMappingURL=805.0f3702d3.chunk.js.map
